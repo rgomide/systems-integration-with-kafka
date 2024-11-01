bbc6402b915595824a4a169344b601d9840642b3
rgomide <renato.s.gomide@gmail.com>
2024-11-01 13:27:56 -0300

    start setup java project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Inventory Service
+
+## Development
+
+```bash
+docker compose up --build
+```
+
+## User Interfaces
+- [Debezium UI](http://localhost:8084)
+- [Redpanda](http://localhost:8081)
--- apps/monitor/src/main/java/br/com/gomide/monitor/MonitorApplication.java
+++ apps/monitor/src/main/java/br/com/gomide/monitor/MonitorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MonitorApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Monitor Application");
 		SpringApplication.run(MonitorApplication.class, args);
 	}
 
